cce35b45933b0d2bbe49f47dda53cbdd4b6d503d
Eric Schultz <eschultzmt@gmail.com>
2025-11-03 20:20:25 -0700

    fix: allow iframe embedding on mobile

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,23 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://hello.formversation.com https://*.formversation.com",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
